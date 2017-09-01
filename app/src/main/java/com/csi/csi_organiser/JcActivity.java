3a265edfcf0a2ff5687c52ecf6fe142c3c12e4de
D3STRON <30839285+D3STRON@users.noreply.github.com>
2017-09-01 14:22:57 +0530

    Done

--- app/src/main/java/com/csi/csi_organiser/JcActivity.java
+++ app/src/main/java/com/csi/csi_organiser/JcActivity.java
@@ -96,7 +96,7 @@ public class JcActivity extends AppCompatActivity {
    tasklist.setOnItemSelectedListener(new AdapterView.OnItemSelectedListener() {
        @Override
        public void onItemSelected(AdapterView<?> parent, View view, int position, long id) {
-                         
+
        }
 
        @Override
